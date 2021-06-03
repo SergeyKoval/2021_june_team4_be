5b3f5f24670a0deb060742c10c91a86f0cdf4b68
Liza <el.dubogryz@gmail.com>
2021-06-03 08:50:07 +0300

    Package structure changed

--- src/main/java/com/exadel/discount/Application.java
+++ src/main/java/com/exadel/discount/Application.java
@@ -1,4 +1,4 @@
-package com.discount;
+package com.exadel.discount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/exadel/discount/controller/TestController.java
+++ src/main/java/com/exadel/discount/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.discount.controller;
+package com.exadel.discount.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
