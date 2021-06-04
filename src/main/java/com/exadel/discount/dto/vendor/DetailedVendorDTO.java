ea591b7c76b5e4cf4b6a273769ca994fb836a586
Liza <el.dubogryz@gmail.com>
2021-06-04 20:53:31 +0300

    Update DetailedVendorDTO.java
    Delete extension of BaseVendorDTO

--- src/main/java/com/exadel/discount/dto/vendor/DetailedVendorDTO.java
+++ src/main/java/com/exadel/discount/dto/vendor/DetailedVendorDTO.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class DetailedVendorDTO extends BaseVendorDTO{
+public class DetailedVendorDTO {
     private UUID id;
     private String name;
     private Set<LocationInfoDTO> locations;
