00316705528b1414ad31346399e7ce6e90de4a8f
Liza <el.dubogryz@gmail.com>
2021-06-07 13:19:51 +0300

    Fix some details in VendorController

--- src/main/java/com/exadel/discount/controller/VendorController.java
+++ src/main/java/com/exadel/discount/controller/VendorController.java
@@ -17,13 +17,13 @@ import java.util.List;
 @RequestMapping("/vendors")
 public class VendorController {
     private final List<DetailedVendorDTO> detailedVendorsServiceMock = List.of(
-            new DetailedVendorDTO(2352434, "Vendor1", List.of(
+            new DetailedVendorDTO(1, "Vendor1", List.of(
                     new LocationInfoDTO(3142342, "Country", "City", "Some address", "Some contacts")
             ), "Description")
     );
 
     private final List<BaseVendorDTO> baseVendorsServiceMock = List.of(
-            new BaseVendorDTO(3534534, "Vendor2")
+            new BaseVendorDTO(2, "Vendor2")
     );
 
     @GetMapping
@@ -36,7 +36,7 @@ public class VendorController {
 
     @GetMapping("/{vendorId}")
     @ApiOperation("Get vendor by ID")
-    public ResponseEntity<DetailedVendorDTO> getVendorById(@PathVariable long id) {
+    public ResponseEntity<DetailedVendorDTO> getVendorById(@PathVariable(name = "vendorId") long id) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(detailedVendorsServiceMock.get(0));
@@ -44,25 +44,31 @@ public class VendorController {
 
     @PostMapping
     @ApiOperation("Save new vendor")
-    public void saveNewVendor(@RequestBody CreateVendorDTO vendor) {
+    public ResponseEntity<DetailedVendorDTO> saveNewVendor(@RequestBody CreateVendorDTO vendor) {
         detailedVendorsServiceMock.add(new DetailedVendorDTO(135463, vendor.getName(), vendor.getLocations(), vendor.getDescription()));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(detailedVendorsServiceMock.get(0));
     }
 
     @PutMapping("/{vendorId}")
     @ApiOperation("Update information about vendor")
-    public void updateVendor(@PathVariable long vendorId, @RequestBody DetailedVendorDTO vendor) {
+    public ResponseEntity<DetailedVendorDTO> updateVendor(@PathVariable(name = "vendorId") long vendorId, @RequestBody DetailedVendorDTO vendor) {
         detailedVendorsServiceMock.add(vendor);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(detailedVendorsServiceMock.get(0));
     }
 
     @DeleteMapping("/{vendorId}")
     @ApiOperation("Delete vendor by ID")
-    public void deleteVendor(@PathVariable long vendorId) {
+    public void deleteVendor(@PathVariable(name = "vendorId") long vendorId) {
 
     }
 
     @GetMapping("/{vendorId}/locations")
     @ApiOperation("Get list of all vendor locations")
-    public ResponseEntity<List<LocationInfoDTO>> getVendorLocations(@PathVariable long vendorId) {
+    public ResponseEntity<List<LocationInfoDTO>> getVendorLocations(@PathVariable(name = "vendorId") long vendorId) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ArrayList<>());
@@ -70,23 +76,23 @@ public class VendorController {
 
     @PostMapping("/{vendorId}/locations")
     @ApiOperation("Add new location of vendor")
-    public void addNewLocation(@PathVariable long vendorId, @RequestBody CreateLocationDTO location) {
-
+    public ResponseEntity<LocationInfoDTO> addNewLocation(@PathVariable(name = "vendorId") long vendorId, @RequestBody CreateLocationDTO location) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new LocationInfoDTO(3142342, "Country", "City", "Some address", "Some contacts"));
     }
 
     @PutMapping("/{vendorId}/locations/{locationId}")
-    @ApiOperation("Update vendor location")
-    public ResponseEntity<LocationInfoDTO> updateLocation(@PathVariable long id, @RequestBody LocationInfoDTO location) {
+    @ApiOperation("Update vendor's location")
+    public ResponseEntity<LocationInfoDTO> updateLocation(@PathVariable(name = "vendorId") long vendorId, @PathVariable(name = "locationId") long locationId, @RequestBody LocationInfoDTO location) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(location);
     }
 
-    @GetMapping("/location")
-    @ApiOperation("Get list of vendors by location (country and city)")
-    public ResponseEntity<List<BaseVendorDTO>> getVendorsByLocation(@RequestParam String country, @RequestParam String city) {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(baseVendorsServiceMock);
+    @DeleteMapping("/{vendorId}/locations/{locationId}")
+    @ApiOperation("Update vendor's location")
+    public void deleteLocation(@PathVariable(name = "vendorId") long id, @PathVariable(name = "locationId") long locationId) {
+
     }
 }
