2214f944ddba292bf30b5f76756c1888575aa5d9
Liza <el.dubogryz@gmail.com>
2021-06-08 22:06:29 +0300

    Add validation in VendorController

--- src/main/java/com/exadel/discount/controller/VendorController.java
+++ src/main/java/com/exadel/discount/controller/VendorController.java
@@ -6,14 +6,16 @@ import com.exadel.discount.dto.vendor.BaseVendorDTO;
 import com.exadel.discount.dto.vendor.CreateVendorDTO;
 import com.exadel.discount.dto.vendor.DetailedVendorDTO;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/vendors")
 public class VendorController {
@@ -35,51 +37,54 @@ public class VendorController {
 
     @GetMapping("/{vendorId}")
     @ApiOperation("Get vendor by ID")
-    public DetailedVendorDTO getVendorById(@PathVariable(name = "vendorId") long id) {
+    public DetailedVendorDTO getVendorById(@PathVariable(name = "vendorId") @Min(value = 0) long id) {
         return detailedVendorsServiceMock.get(0);
     }
 
     @PostMapping
     @ApiOperation("Save new vendor")
-    public DetailedVendorDTO saveNewVendor(@RequestBody CreateVendorDTO vendor) {
-        detailedVendorsServiceMock.add(new DetailedVendorDTO(135463, vendor.getName(), vendor.getLocations(), vendor.getDescription()));
+    public DetailedVendorDTO saveNewVendor(@RequestBody @Valid CreateVendorDTO vendor) {
         return detailedVendorsServiceMock.get(0);
     }
 
     @PutMapping("/{vendorId}")
     @ApiOperation("Update information about vendor")
-    public DetailedVendorDTO updateVendor(@PathVariable(name = "vendorId") long vendorId, @RequestBody DetailedVendorDTO vendor) {
-        detailedVendorsServiceMock.add(vendor);
-        return detailedVendorsServiceMock.get(0);
+    public DetailedVendorDTO updateVendor(@PathVariable(name = "vendorId") @Min(value = 0) long vendorId,
+                                          @RequestBody @Valid DetailedVendorDTO vendor) {
+        return vendor;
     }
 
     @DeleteMapping("/{vendorId}")
     @ApiOperation("Delete vendor by ID")
-    public void deleteVendor(@PathVariable(name = "vendorId") long vendorId) {
+    public void deleteVendor(@PathVariable(name = "vendorId") @Min(value = 0) long vendorId) {
 
     }
 
     @GetMapping("/{vendorId}/locations")
     @ApiOperation("Get list of all vendor locations")
-    public List<LocationInfoDTO> getVendorLocations(@PathVariable(name = "vendorId") long vendorId) {
+    public List<LocationInfoDTO> getVendorLocations(@PathVariable(name = "vendorId") @Min(value = 0) long vendorId) {
         return new ArrayList<>();
     }
 
     @PostMapping("/{vendorId}/locations")
     @ApiOperation("Add new location of vendor")
-    public LocationInfoDTO addNewLocation(@PathVariable(name = "vendorId") long vendorId, @RequestBody CreateLocationDTO location) {
+    public LocationInfoDTO addNewLocation(@PathVariable(name = "vendorId") @Min(value = 0) long vendorId,
+                                          @RequestBody @Valid CreateLocationDTO location) {
         return new LocationInfoDTO(3142342, "Country", "City", "Some address", "Some contacts");
     }
 
     @PutMapping("/{vendorId}/locations/{locationId}")
     @ApiOperation("Update vendor's location")
-    public LocationInfoDTO updateLocation(@PathVariable(name = "vendorId") long vendorId, @PathVariable(name = "locationId") long locationId, @RequestBody LocationInfoDTO location) {
+    public LocationInfoDTO updateLocation(@PathVariable(name = "vendorId") @Min(value = 0) long vendorId,
+                                          @PathVariable(name = "locationId") @Min(value = 0) long locationId,
+                                          @RequestBody @Valid LocationInfoDTO location) {
         return location;
     }
 
     @DeleteMapping("/{vendorId}/locations/{locationId}")
     @ApiOperation("Delete vendor's location")
-    public void deleteLocation(@PathVariable(name = "vendorId") long id, @PathVariable(name = "locationId") long locationId) {
+    public void deleteLocation(@PathVariable(name = "vendorId") @Min(value = 0) long id,
+                               @PathVariable(name = "locationId") @Min(value = 0) long locationId) {
 
     }
 }
