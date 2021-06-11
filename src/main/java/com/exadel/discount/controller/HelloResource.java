ed2dfcb7dc78808b9837e61d6b05073ae3258107
Artem-Kishenets <artem_kishenets@icloud.com>
2021-06-11 16:22:00 +0300

    initial configuration for security and jwt

---
+++ src/main/java/com/exadel/discount/controller/HelloResource.java
@@ -0,0 +1,46 @@
+package com.exadel.discount.controller;
+
+import com.exadel.discount.model.AuthenticationRequest;
+import com.exadel.discount.model.AuthenticationResponse;
+import com.exadel.discount.service.MyUserDetailsService;
+import com.exadel.discount.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+public class HelloResource {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello world!!";
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
