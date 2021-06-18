9ab7cc99cc37370d721ccfc909453b6ba78f6214
Liza <el.dubogryz@gmail.com>
2021-06-18 16:28:22 +0300

    Fixing exception handling

--- src/main/java/com/exadel/discount/exception/GlobalExceptionHandler.java
+++ src/main/java/com/exadel/discount/exception/GlobalExceptionHandler.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
@@ -15,9 +18,21 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionDetails> handleException(MethodArgumentNotValidException notValidException) {
-        String message = notValidException.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        ExceptionDetails exceptionDetails = new ExceptionDetails(message);
+    public ResponseEntity<List<ExceptionDetails>> handleException(
+            MethodArgumentNotValidException notValidException) {
+        List<ExceptionDetails> exceptionDetailsList = notValidException
+                .getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> new ExceptionDetails(error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(exceptionDetailsList, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionDetails> handleException(
+            Exception exception) {
+        ExceptionDetails exceptionDetails = new ExceptionDetails(exception.getMessage());
         return new ResponseEntity<>(exceptionDetails, HttpStatus.BAD_REQUEST);
     }
 }
