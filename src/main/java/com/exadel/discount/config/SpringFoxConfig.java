500083f59debbbb3776326a9f6d273d9f6178afc
hanna <hanna.kalashnik@gmail.com>
2021-06-18 16:53:37 +0300

    +SpringFoxConfig

---
+++ src/main/java/com/exadel/discount/config/SpringFoxConfig.java
@@ -0,0 +1,28 @@
+package com.exadel.discount.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder()
+                        .title("Discount API")
+                        .description("API for the Discount Application")
+                        .version("1.0")
+                        .build()
+                );
+    }
+}
