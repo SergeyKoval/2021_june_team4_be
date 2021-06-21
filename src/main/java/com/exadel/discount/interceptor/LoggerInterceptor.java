c80317e28560b776f055cb3516bb28bbcff75fbb
Liza <el.dubogryz@gmail.com>
2021-06-21 13:33:16 +0300

    Update LoggerInterceptor.java

--- src/main/java/com/exadel/discount/interceptor/LoggerInterceptor.java
+++ src/main/java/com/exadel/discount/interceptor/LoggerInterceptor.java
@@ -2,16 +2,21 @@ package com.exadel.discount.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Slf4j
+@Component
 public class LoggerInterceptor implements HandlerInterceptor {
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         MDC.clear();
+        final UUID requestId = UUID.randomUUID();
+        MDC.put("requestId", requestId.toString());
+        return true;
     }
 }
