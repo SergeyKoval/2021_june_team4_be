a324630ce2e69be379ecce2f9ab8a151cf7789aa
Liza <el.dubogryz@gmail.com>
2021-06-21 15:30:15 +0300

    Add Qualifier

--- src/main/java/com/exadel/discount/config/WebMvcConfig.java
+++ src/main/java/com/exadel/discount/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.exadel.discount.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,10 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
-    private final HandlerInterceptor handlerInterceptor;
+    @Qualifier("loggerInterceptor")
+    private final HandlerInterceptor loggerInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(handlerInterceptor);
+        registry.addInterceptor(loggerInterceptor);
     }
 }
