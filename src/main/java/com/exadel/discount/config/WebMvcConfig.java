fb6f9a79e30cb67ec10513da4204dab6d665098a
Liza <el.dubogryz@gmail.com>
2021-06-21 13:32:44 +0300

    Update WebMvcConfig.java

--- src/main/java/com/exadel/discount/config/WebMvcConfig.java
+++ src/main/java/com/exadel/discount/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.exadel.discount.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
     private final HandlerInterceptor handlerInterceptor;
