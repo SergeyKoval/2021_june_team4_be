c852dbea5959db54584cf306dfce053491cda83d
Liza <el.dubogryz@gmail.com>
2021-06-24 18:22:51 +0300

    Create UserRepository.java

---
+++ src/main/java/com/exadel/discount/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.exadel.discount.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByEmail(String email);
+}
