7168006033da9f15f368b131df540c853cc7831e
Anton Rud <professor.toxa@gmail.com>
2021-06-24 12:41:54 +0300

    Edit some class, delete test annotantions

--- src/test/java/com/exadel/discount/DiscountApplicationTests.java
+++ src/test/java/com/exadel/discount/DiscountApplicationTests.java
@@ -1,15 +1,5 @@
 package com.exadel.discount;
 
-import com.exadel.discount.controller.TestControllerMockMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
 
 public class DiscountApplicationTests {
 
--- src/test/java/com/exadel/discount/controller/TestContainersTest.java
+++ src/test/java/com/exadel/discount/controller/TestContainersTest.java
@@ -1,10 +1,16 @@
 package com.exadel.discount.controller;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.exadel.discount.entity.Tag;
+import com.exadel.discount.repository.TagRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,23 +18,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-import java.io.File;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-@ContextConfiguration(initializers = {TestControllerMockMvcTest.Initializer.class})
+@ContextConfiguration(initializers = {TestContainersTest.Initializer.class})
 @Testcontainers
-public class TestControllerMockMvcTest {
+public class TestContainersTest {
+    @Autowired
+    TagRepository tagRepository;
+
     @Container
     public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:12-alpine")
             .withDatabaseName("mydb")
@@ -36,24 +41,16 @@ public class TestControllerMockMvcTest {
             .withPassword("mypass");
 
 
-    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext>{
-        public void initialize(ConfigurableApplicationContext context){
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext context) {
             TestPropertyValues.of(
-                        "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-                        "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-                        "spring.datasource.password=" + postgreSQLContainer.getPassword()
-                       // "spring.liquibase.enabled=true"
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword(),
+                    "spring.liquibase.enabled=false"
             ).applyTo(context.getEnvironment());
         }
     }
-    @Autowired
-    private MockMvc mockMvc;
 
-    @Test
-    public void shouldReturnDefaultMessage() throws Exception{
-        this.mockMvc.perform(get("/test"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, world")));
-    }
+
 }
