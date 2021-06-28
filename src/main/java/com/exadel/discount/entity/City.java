a85842f62531b92c4d088e5a747c372820dec498
hanna <hanna.kalashnik@gmail.com>
2021-06-28 18:44:45 +0300

    works

---
+++ src/main/java/com/exadel/discount/entity/City.java
@@ -0,0 +1,48 @@
+package com.exadel.discount.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+import java.util.UUID;
+@Entity
+@Table(name = "cities")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {"users"})
+@ToString(exclude = {"users"})
+public class City {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="country_id", nullable=false)
+    private Country country;
+
+    @OneToMany(mappedBy = "city", fetch = FetchType.LAZY)
+    private List<User> users;
+
+//    @OneToMany(mappedBy = "city", fetch = FetchType.LAZY)
+//    private List<VendorLocation> vendorLocations;
+}
---
+++ src/main/java/com/exadel/discount/entity/Country.java
@@ -0,0 +1,44 @@
+package com.exadel.discount.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "countries")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {"cities"})
+@ToString(exclude = {"cities"})
+public class Country {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    String name;
+
+    @OneToMany(mappedBy = "country", fetch = FetchType.LAZY)
+    private Set<City> cities;
+
+//    @OneToMany(mappedBy = "country", fetch = FetchType.LAZY)
+//    private List<VendorLocation> vendorLocations;
+}
