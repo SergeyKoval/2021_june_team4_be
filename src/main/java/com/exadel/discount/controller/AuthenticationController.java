b81a97c8486ef7883e767974aef1e9147ffe8f20
Artem-Kishenets <artem_kishenets@icloud.com>
2021-07-01 00:01:04 +0300

    an unused import was deleted

--- src/main/java/com/exadel/discount/controller/AuthenticationController.java
+++ src/main/java/com/exadel/discount/controller/AuthenticationController.java
@@ -12,7 +12,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
