1ab5891f67ae3a7af01338536137b2e07a2dd59e
Nick <n.chornyi666@gmail.com>
2021-07-02 14:04:28 +0300

    Add Repositories & Service interfaces & Mapper (Vendor, Location, Discount, Category)

---
+++ src/main/java/com/exadel/discount/service/CategoryService.java
@@ -0,0 +1,17 @@
+package com.exadel.discount.service;
+
+import com.exadel.discount.dto.CategoryDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CategoryService {
+
+    CategoryDTO save(CategoryDTO categoryDTO);
+
+    CategoryDTO getById(UUID id);
+
+    void deleteById(UUID id);
+
+    List<CategoryDTO> getAll();
+}
