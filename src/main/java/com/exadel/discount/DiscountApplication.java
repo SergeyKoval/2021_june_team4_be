572a4afb8ad3285b542bf0be4995332aad71ab11
hanna <hanna.kalashnik@gmail.com>
2021-07-05 06:58:16 +0300

    works. Needs better properties into messages_XX.properties

--- src/main/java/com/exadel/discount/DiscountApplication.java
+++ src/main/java/com/exadel/discount/DiscountApplication.java
@@ -2,11 +2,9 @@ package com.exadel.discount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableAutoConfiguration(exclude = {WebMvcAutoConfiguration.class })
