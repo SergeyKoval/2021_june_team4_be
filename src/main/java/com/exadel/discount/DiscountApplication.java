831767afef4709031aae7006207a3ecdf20525b6
hanna <hanna.kalashnik@gmail.com>
2021-07-05 08:00:35 +0300

    rollBack of DiscountApplication.java

--- src/main/java/com/exadel/discount/DiscountApplication.java
+++ src/main/java/com/exadel/discount/DiscountApplication.java
@@ -1,14 +1,9 @@
 package com.exadel.discount;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration(exclude = {WebMvcAutoConfiguration.class })
-@ComponentScan
+@SpringBootApplication
 public class DiscountApplication {
 
     public static void main(String[] args) {
