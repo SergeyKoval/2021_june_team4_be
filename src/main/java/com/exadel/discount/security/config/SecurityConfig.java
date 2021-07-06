485fb1268baed626dcd6a9508639f52f0f746c44
Artem-Kishenets <artem_kishenets@icloud.com>
2021-07-06 21:52:23 +0300

    cors configuration has been upgraded

--- src/main/java/com/exadel/discount/security/config/SecurityConfig.java
+++ src/main/java/com/exadel/discount/security/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/authenticate/login").permitAll()
