794a124783206e759abef2d816cd35fb536ce925
Anton Rud <professor.toxa@gmail.com>
2021-07-06 22:08:24 +0300

    Edit mapping in VendorLocationMapper thx Liza

--- src/main/java/com/exadel/discount/mapper/VendorLocationMapper.java
+++ src/main/java/com/exadel/discount/mapper/VendorLocationMapper.java
@@ -4,14 +4,19 @@ import com.exadel.discount.dto.location.CreateLocationDTO;
 import com.exadel.discount.dto.location.LocationDTO;
 import com.exadel.discount.entity.VendorLocation;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface VendorLocationMapper {
 
+    @Mapping(source = "latitude", target = "position.latitude")
+    @Mapping(source = "longitude",target = "position.longitude")
     VendorLocation parseDTO(LocationDTO vendorLocationDTO);
 
+    @Mapping(source = "latitude", target = "position.latitude")
+    @Mapping(source = "longitude",target = "position.longitude")
     VendorLocation parseDTO(CreateLocationDTO createLocationDTO);
 
     LocationDTO getDTO(VendorLocation vendorLocation);
