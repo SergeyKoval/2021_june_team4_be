31b681265593d3736eb94b3fcadcdbbdbc0ba14a
Elizaveta Dubogryz <54329787+el-dub@users.noreply.github.com>
2021-07-07 16:22:52 +0300

    BE-018-2 City country (repository, service, controller) (#56)
    * Added entities City and Country
    
    * Added DTO and Mapper
    
    * Added Repository and Service
    
    * Service impl
    
    * Fixed error
    
    * Added logging and method findAllCitiesByCountryId()
    
    * Exception handling during deleting
    
    * @Column annotation
    
    * Added comment
    
    * Added @EqualsAndHashCode and @ToString annotations
    
    * Changed deleteById methods
    
    * Changed CityDTO and CountryDTO
    
    * Changed CityMapper, CountryMapper, repository and service. Added City and Country Controllers for testing
    
    * Some changes after Hanna's comments
    
    * Deleted commented code
    
    * Deleted commented code
    
    * Added fields:
    private Country country;
    private City city;
     to VendorLocation entity
    
    * Changed and moved endpoints from CityController to CountryController
    
    * Formatting
    
    * Update City Country DTO
    
    * Update entities
    
    * Update mappers
    
    * Update location DTO
    
    * Update VendorRepository.java
    
    * Update VendorLocationRepository.java
    
    * Update DiscountRepository.java
    
    * Update CityRepository.java
    
    * Update saving new City
    
    * Format code
    
    * Add access annotations
    
    * Update CountryController.java
    
    Co-authored-by: Serhii Pakulia <serhii.pakulia@gmail.com>

---
+++ src/main/java/com/exadel/discount/service/CountryService.java
@@ -0,0 +1,19 @@
+package com.exadel.discount.service;
+
+import com.exadel.discount.dto.CountryDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CountryService {
+
+    List<CountryDTO> findAll();
+
+    CountryDTO findById(UUID id);
+
+    CountryDTO findByName(String name);
+
+    CountryDTO save(CountryDTO countryDTO);
+
+    void deleteById(UUID id);
+}
