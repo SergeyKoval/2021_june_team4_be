acaea9d7f5011a11ab8daa3d754268bc4cb4d236
Liza <el.dubogryz@gmail.com>
2021-07-07 14:55:43 +0300

    Update CityRepository.java

--- src/main/java/com/exadel/discount/repository/CityRepository.java
+++ src/main/java/com/exadel/discount/repository/CityRepository.java
@@ -1,9 +1,8 @@
 package com.exadel.discount.repository;
 
 import com.exadel.discount.entity.City;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,9 +11,16 @@ import java.util.UUID;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, UUID> {
+
+    @EntityGraph(attributePaths = {"country"})
+    Optional<City> findById(UUID id);
+
+    @EntityGraph(attributePaths = {"country"})
     List<City> findByCountryName(String name);
 
+    @EntityGraph(attributePaths = {"country"})
     List<City> findByCountryId(UUID uuid);
 
+    @EntityGraph(attributePaths = {"country"})
     Optional<City> findByName(String name);
 }
