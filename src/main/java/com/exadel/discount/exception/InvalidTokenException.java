5ff41f4bb642cfc287e7e933e2002cfd91dcfdf1
Liza <el.dubogryz@gmail.com>
2021-07-07 11:49:19 +0300

    Format code

--- src/main/java/com/exadel/discount/exception/InvalidTokenException.java
+++ src/main/java/com/exadel/discount/exception/InvalidTokenException.java
@@ -1,7 +1,7 @@
 package com.exadel.discount.exception;
 
 public class InvalidTokenException extends RuntimeException {
-    public InvalidTokenException(String message){
+    public InvalidTokenException(String message) {
         super(message);
     }
 }
