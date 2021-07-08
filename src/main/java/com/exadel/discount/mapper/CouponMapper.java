e5437bcc4cad7d4ff2b035dd7f92281160f2bb85
hanna <hanna.kalashnik@gmail.com>
2021-07-08 14:17:50 +0300

    Coupon/Favorite mapper for check uot

--- src/main/java/com/exadel/discount/mapper/CouponMapper.java
+++ src/main/java/com/exadel/discount/mapper/CouponMapper.java
@@ -3,21 +3,12 @@ package com.exadel.discount.mapper;
 import com.exadel.discount.dto.coupon.CouponDTO;
 import com.exadel.discount.entity.Coupon;
 import org.mapstruct.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
-public abstract class CouponMapper{
-    @Autowired
-    DiscountMapper discountMapper;
-    public CouponDTO toCouponDTO(Coupon coupon){
-        CouponDTO couponDTO = new CouponDTO();
-        couponDTO.setDate(coupon.getDate());
-        couponDTO.setId(coupon.getId());
-        couponDTO.setDiscountDTO(discountMapper.getDTO(coupon.getDiscount()));
-        return couponDTO;
-    }
+@Mapper(componentModel = "spring", uses = {DiscountMapper.class})
+public interface CouponMapper{
+    CouponDTO toCouponDTO(Coupon coupon);
 
-    public abstract List<CouponDTO> toCouponDTOList(List<Coupon> coupons);
+    List<CouponDTO> toCouponDTOList(List<Coupon> coupons);
 }
