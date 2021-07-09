75be1a709f8fd57af8b846b823dd29bfc7763f09
Anton Rud <professor.toxa@gmail.com>
2021-07-09 15:35:08 +0300

    Optimize imports

--- src/main/java/com/exadel/discount/dto/discount/CreateDiscountDTO.java
+++ src/main/java/com/exadel/discount/dto/discount/CreateDiscountDTO.java
@@ -3,7 +3,6 @@ package com.exadel.discount.dto.discount;
 import com.exadel.discount.entity.DiscountType;
 import lombok.Data;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
