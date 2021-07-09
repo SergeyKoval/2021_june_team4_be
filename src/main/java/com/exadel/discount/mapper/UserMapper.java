ee493623b52b47c515b344b8ac76ccb63f1d58c4
hanna <hanna.kalashnik@gmail.com>
2021-07-09 17:08:54 +0300

    ups

--- src/main/java/com/exadel/discount/mapper/UserMapper.java
+++ src/main/java/com/exadel/discount/mapper/UserMapper.java
@@ -1,12 +1,7 @@
 package com.exadel.discount.mapper;
 
-import com.exadel.discount.dto.city.CityDTO;
-import com.exadel.discount.dto.country.CountryDTO;
 import com.exadel.discount.dto.user.UserDTO;
-import com.exadel.discount.entity.City;
-import com.exadel.discount.entity.Country;
 import com.exadel.discount.entity.User;
-import lombok.AllArgsConstructor;
 import org.mapstruct.Mapper;
 
 import java.util.List;
