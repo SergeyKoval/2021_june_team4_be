c8d217f962f3cdc65511526927429016f7f99912
hanna <hanna.kalashnik@gmail.com>
2021-07-10 11:39:01 +0300

    +querydsl

--- src/main/java/com/exadel/discount/service/FavoriteService.java
+++ src/main/java/com/exadel/discount/service/FavoriteService.java
@@ -1,20 +1,23 @@
 package com.exadel.discount.service;
 
-import com.exadel.discount.dto.favorite.CreateFavoriteDto;
-import com.exadel.discount.dto.favorite.FavoriteDto;
+import com.exadel.discount.dto.favorite.CreateFavoriteDTO;
+import com.exadel.discount.dto.favorite.FavoriteDTO;
+import com.exadel.discount.dto.favorite.FavoriteFilter;
 
 
 import java.util.List;
 import java.util.UUID;
 
 public interface FavoriteService {
-    FavoriteDto findFavoriteById(UUID id);
+    FavoriteDTO findFavoriteById(UUID id);
 
     void deleteFavoriteByID(UUID id);
 
-    List<FavoriteDto> findAllFavorites( int pageNumber, int pageSize, String sortDirection, String sortField);
+    FavoriteDTO assignFavoriteToUser(CreateFavoriteDTO createFavoriteDTO);
 
-    FavoriteDto assignFavoriteToUser(CreateFavoriteDto createFavoriteDto);
-
-    List<FavoriteDto> getFavoritesOfUser(int pageNumber, int pageSize, String sortDirection, String sortField, UUID userId);
+    List<FavoriteDTO> findAllFavorites(int pageNumber,
+                                       int pageSize,
+                                       String sortDirection,
+                                       String sortField,
+                                       FavoriteFilter filter);
 }
