a6800d4a2f86d594041553e4e1c164dac2bc5903
Liza <el.dubogryz@gmail.com>
2021-07-12 13:53:17 +0300

    Add QuerydslConfig

--- src/main/java/com/exadel/discount/repository/impl/QueryFactoryDiscountRepositoryImpl.java
+++ src/main/java/com/exadel/discount/repository/impl/QueryFactoryDiscountRepositoryImpl.java
@@ -4,21 +4,18 @@ import com.exadel.discount.entity.QDiscount;
 import com.exadel.discount.repository.QueryFactoryDiscountRepository;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
+@RequiredArgsConstructor
 public class QueryFactoryDiscountRepositoryImpl implements QueryFactoryDiscountRepository {
 
     private final JPAQueryFactory queryFactory;
-
-    public QueryFactoryDiscountRepositoryImpl(EntityManager em) {
-        this.queryFactory = new JPAQueryFactory(em);
-    }
 
     @Override
     public List<UUID> findAllDiscountIds(Predicate predicate, Pageable pageable) {
