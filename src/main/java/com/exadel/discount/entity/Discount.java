a97ed89b94df1c7e430b8c2b2d8226b5a6c37637
hanna <hanna.kalashnik@gmail.com>
2021-07-12 13:23:04 +0300

    *

--- src/main/java/com/exadel/discount/entity/Discount.java
+++ src/main/java/com/exadel/discount/entity/Discount.java
@@ -9,7 +9,20 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
