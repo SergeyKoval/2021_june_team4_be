c6f89b92e745ea88ed27f634e334d19e1836443c
hanna <hanna.kalashnik@gmail.com>
2021-07-13 10:12:58 +0300

    city/country->listsIds in UserFilter

--- src/main/java/com/exadel/discount/model/dto/user/UserFilter.java
+++ src/main/java/com/exadel/discount/model/dto/user/UserFilter.java
@@ -4,12 +4,16 @@ import com.exadel.discount.model.entity.Role;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+import java.util.UUID;
+
 @Data
 @Builder
 public class UserFilter {
         private String firstName;
         private String lastName;
         private Role role;
-        private String cityName;
-        private String countryName;
+        private List<UUID> cityIds;
+        private List<UUID> countryIds;
+
 }
