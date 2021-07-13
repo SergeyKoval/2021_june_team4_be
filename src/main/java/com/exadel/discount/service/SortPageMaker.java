fa49ceca6252474715c3cdaa45185a23eb272d43
hanna <hanna.kalashnik@gmail.com>
2021-07-13 10:50:49 +0300

    SortPageMaker

--- src/main/java/com/exadel/discount/service/SortPageMaker.java
+++ src/main/java/com/exadel/discount/service/SortPageMaker.java
@@ -6,10 +6,10 @@ import org.springframework.data.domain.Sort;
 
 public class SortPageMaker {
     public static Pageable makePageable(int pageNumber, int pageSize, String sortDirection, String sortField) {
-    Sort sort = Sort.unsorted();
-    if (!sortDirection.equals("")) {
-        sort = Sort.by(Sort.Direction.valueOf(sortDirection.toUpperCase()), sortField);
+        Sort sort = Sort.unsorted();
+        if (sortDirection.equalsIgnoreCase("ASC") || (sortDirection.equalsIgnoreCase("DSC"))) {
+            sort = Sort.by(Sort.Direction.valueOf(sortDirection.toUpperCase()), sortField);
+        }
+        return PageRequest.of(pageNumber - 1, pageSize, sort);
     }
-    return PageRequest.of(pageNumber - 1, pageSize, sort);
-}
 }
