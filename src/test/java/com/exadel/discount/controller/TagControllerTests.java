072a94ced5ffcedbffd40d6c0fef29b17aef78cf
Anton Rud <professor.toxa@gmail.com>
2021-07-14 17:28:52 +0300

    Deleted config package, use only one Spring Annotation

--- src/test/java/com/exadel/discount/controller/TagControllerTests.java
+++ src/test/java/com/exadel/discount/controller/TagControllerTests.java
@@ -1,16 +1,19 @@
 package com.exadel.discount.controller;
 
+import com.exadel.discount.config.WithMockCustomUser;
 import com.exadel.discount.service.TagService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser(username="admin",roles={"USER","ADMIN"})
 public class TagControllerTests {
     @Autowired
     private MockMvc mockMvc;
