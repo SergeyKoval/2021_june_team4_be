084eb3ca1233aadcb5099d45c901bac7569546a6
Liza <el.dubogryz@gmail.com>
2021-07-15 12:10:24 +0300

    Add location filtering for discounts getAll()

--- src/main/java/com/exadel/discount/service/impl/DiscountServiceImpl.java
+++ src/main/java/com/exadel/discount/service/impl/DiscountServiceImpl.java
@@ -89,8 +89,7 @@ public class DiscountServiceImpl implements DiscountService {
         List<UUID> discountIds = discountRepository
                 .findAllDiscountIds(preparePredicateForFindingAll(filter), PageRequest.of(page, size));
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-        List<DiscountDTO> discountDTOS = discountRepository
-                .findAllByIdInWithFavoritesByUser(discountIds, sort, userEmail)
+        List<DiscountDTO> discountDTOS = findAllDiscounts(discountIds, sort, userEmail, filter)
                 .stream()
                 .map(discount -> {
                     DiscountDTO discountDTO = discountMapper.getDTO(discount);
@@ -210,5 +209,18 @@ public class DiscountServiceImpl implements DiscountService {
                         .buildOr())
                 .collect(Collectors.toList());
         return ExpressionUtils.allOf(searchPredicates);
+    }
+
+    private List<Discount> findAllDiscounts(List<UUID> discountIds, Sort sort, String userEmail,
+                                            DiscountFilter filter) {
+        if (filter.getCityIds() != null || filter.getCountryIds() != null) {
+            return discountRepository
+                    .findAllByIdInWithFavoritesByUserAndLocations(discountIds, sort, userEmail,
+                            filter.getCityIds(), filter.getCountryIds());
+        } else {
+            return discountRepository
+                    .findAllByIdInWithFavoritesByUser(discountIds, sort, userEmail);
+        }
+
     }
 }
