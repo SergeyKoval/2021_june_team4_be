e202cac189de1d350d588c7a0028b3bd561ab3b3
Liza <el.dubogryz@gmail.com>
2021-07-19 17:07:13 +0300

    Move default values to constants

--- src/main/java/com/exadel/discount/model/dto/statistics/StatisticsFilter.java
+++ src/main/java/com/exadel/discount/model/dto/statistics/StatisticsFilter.java
@@ -10,6 +10,9 @@ import java.util.UUID;
 @Data
 @Builder
 public class StatisticsFilter {
+    private static final String DEFAULT_DATE_FROM = "2000-01-01T00:00:00";
+    private static final String DEFAULT_DATE_TO = "3000-01-01T00:00:00";
+
     private List<UUID> countryIds;
     private List<UUID> cityIds;
     private LocalDateTime dateFrom;
@@ -17,10 +20,10 @@ public class StatisticsFilter {
 
     public StatisticsFilter build() {
         if (dateFrom == null) {
-            dateFrom = LocalDateTime.parse("2000-01-01T00:00:00");
+            dateFrom = LocalDateTime.parse(DEFAULT_DATE_FROM);
         }
         if (dateTo == null) {
-            dateFrom = LocalDateTime.parse("3000-01-01T00:00:00");
+            dateFrom = LocalDateTime.parse(DEFAULT_DATE_TO);
         }
         return new StatisticsFilter(countryIds, cityIds, dateFrom, dateTo);
     }
