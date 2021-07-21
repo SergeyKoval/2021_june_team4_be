497c250cdd7938c83660eae0cfbcf1d70a33a2aa
hanna <hanna.kalashnik@gmail.com>
2021-07-21 10:03:27 +0300

    alter test container

---
+++ src/test/java/com/exadel/discount/controller/alterTestContainer/TestPostgresContainer.java
@@ -0,0 +1,45 @@
+package com.exadel.discount.controller.alterTestContainer;
+
+import com.exadel.discount.controller.AbstractIT;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = AbstractIT.DockerPostgreDataSourceInitializer.class)
+@Testcontainers
+public class TestPostgresContainer extends PostgreSQLContainer<TestPostgresContainer> {
+
+    private static final String IMAGE_VERSION = "postgres:11.1";
+    private static TestPostgresContainer container;
+
+    private TestPostgresContainer() {
+        super(IMAGE_VERSION);
+    }
+
+    public static TestPostgresContainer getInstance() {
+        if (container == null) {
+            container = new TestPostgresContainer();
+        }
+        return container;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        System.setProperty("DB_URL", container.getJdbcUrl());
+        System.setProperty("DB_USERNAME", container.getUsername());
+        System.setProperty("DB_PASSWORD", container.getPassword());
+    }
+
+    @Override
+    public void stop() {
+        //do nothing, JVM handles shut down
+    }
+
+}
