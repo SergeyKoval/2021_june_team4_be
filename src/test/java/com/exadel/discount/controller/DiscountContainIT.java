ab039c69da1e0e107d1b4015b5ab7bd616a5a636
hanna <hanna.kalashnik@gmail.com>
2021-07-23 10:06:46 +0300

    no unnecessary tests in Discount

--- src/test/java/com/exadel/discount/controller/DiscountContainIT.java
+++ src/test/java/com/exadel/discount/controller/DiscountContainIT.java
@@ -1,10 +1,12 @@
 package com.exadel.discount.controller;
 
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ActiveProfiles;
@@ -12,21 +14,21 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.UUID;
-
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Sql("classpath:test_db.sql")
+@WithMockUser(username = "admin@mail.com", roles = {"USER", "ADMIN"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DiscountContainIT extends AbstractIT {
 
     @Autowired
     WebApplicationContext wac;
 
+    @Order(1)
     @Test
-    @WithMockUser(username = "admin@mail.com", roles = {"USER", "ADMIN"})
     void addDiscountIT() throws Exception {
 
         given()
@@ -48,7 +50,6 @@ public class DiscountContainIT extends AbstractIT {
                 .when()
                 .post("/discounts")
                 .then()
-                // .statusCode(201)
                 .assertThat(MockMvcResultMatchers.status().isOk())
                 .assertThat(MockMvcResultMatchers.status().is2xxSuccessful())
                 .assertThat(jsonPath("$.name").value("Discount on tennis weekdays"))
@@ -56,7 +57,7 @@ public class DiscountContainIT extends AbstractIT {
     }
 
     @Test
-    @WithMockUser
+    @Order(2)
     public void retrieveDiscountByIdIT() throws Exception {
 
         given()
@@ -65,12 +66,145 @@ public class DiscountContainIT extends AbstractIT {
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .when()
-                .get("/discounts/{id}", UUID.fromString("93577f24-f68f-403e-aa04-0a60c3a445d1"))
+                .get("/discounts/93577f24-f68f-403e-aa04-0a60c3a445d1")
                 .then()
-                .status(HttpStatus.OK)
                 .assertThat(MockMvcResultMatchers.status().isOk())
                 .assertThat(MockMvcResultMatchers.status().is2xxSuccessful())
                 .assertThat(jsonPath("$.name").value("Toy for your dog"))
                 .assertThat(jsonPath("$.vendor.name").value("Dog stuff"));
+    }
+
+    @Test
+    @Order(3)
+    void findAllDiscountsIT() throws Exception {
+        given()
+                .webAppContextSetup(wac)
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/discounts")
+                .then()
+                .assertThat(MockMvcResultMatchers.status().isOk())
+                .assertThat(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+
+    @Test
+    @Order(4)
+    void findAllDiscountsWithFiltersIT() throws Exception {
+        given()
+                .webAppContextSetup(wac)
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .contentType("application/json")
+                .param("categoryId", "93577f24-f68f-403e-aa04-0a60c3a445d6")
+                .param("vendorId", "93577f24-f68f-403e-aa04-0a60c3a445d2")
+                .param("vendorLocationsIds", "93577f24-f68f-403e-aa04-0a60c3a445d3",
+                        "93577f24-f68f-403e-aa04-0a60c3a445d4")
+                .param("tagIds", "93577f24-f68f-403e-aa04-0a60c3a445d65")
+                .param("LocalDateTime endDateTimeFrom", "2021-07-19T23:50:00")
+                .param("LocalDateTime endDateTimeTo", "2021-07-20T00:10:00")
+                .when()
+                .get("/discounts")
+                .then()
+                .assertThat(MockMvcResultMatchers.status().isOk())
+                .assertThat(MockMvcResultMatchers.status().is2xxSuccessful())
+                .assertThat(jsonPath("$[0].name").value("Toy for your dog"))
+                .assertThat(jsonPath("$[0].description").value("Dog stuff"))
+                .assertThat(jsonPath("$[0].value").value("5"))
+                .assertThat(jsonPath("$[0].discountType").value("PERCENT"))
+                .assertThat(jsonPath("$[0].promo").value("1abcde1"))
+                .assertThat(jsonPath("$[0].endTime").value("2021-07-20T00:00:00"))
+                .assertThat(jsonPath("$[0].archived").value("false"))
+                .assertThat(jsonPath("$[0].vendor.name").value("Dog stuff"))
+                .assertThat(jsonPath("$[0].category.name").value("Dogs"));
+    }
+
+    @Test
+    @Order(5)
+    public void getDiscountSearchingIT() throws Exception {
+        given().webAppContextSetup(wac)
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .contentType("application/json")
+                .param("searchText", "Toy for")
+                .when()
+                .get("/discounts/search")
+                .then()
+                .assertThat(MockMvcResultMatchers.status().isOk())
+                .assertThat(MockMvcResultMatchers.status().is2xxSuccessful())
+                .assertThat(jsonPath("$[0].name").value("Toy for your dog"))
+                .assertThat(jsonPath("$[0].description").value("Dog stuff"))
+                .assertThat(jsonPath("$[0].value").value("5"))
+                .assertThat(jsonPath("$[0].discountType").value("PERCENT"))
+                .assertThat(jsonPath("$[0].promo").value("1abcde1"))
+                .assertThat(jsonPath("$[0].endTime").value("2021-07-20T00:00:00"))
+                .assertThat(jsonPath("$[0].archived").value("false"))
+                .assertThat(jsonPath("$[0].vendor.name").value("Dog stuff"))
+                .assertThat(jsonPath("$[0].category.name").value("Dogs"));
+    }
+
+    @Test
+    @Order(6)
+    public void deleteDiscountAndCheckIT() throws Exception {
+        given()
+                .webAppContextSetup(wac)
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .contentType("application/json")
+                .auth().with(SecurityMockMvcRequestPostProcessors.user("admin@mail.com").roles("ADMIN"))
+                .when()
+                .delete("/discounts/93577f24-f68f-403e-aa04-0a60c3a445d1")
+                .then()
+                .assertThat(MockMvcResultMatchers.status().is2xxSuccessful());
+
+        given()
+                .webAppContextSetup(wac)
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .param("categoryId", "93577f24-f68f-403e-aa04-0a60c3a445d6")
+                .param("vendorId", "93577f24-f68f-403e-aa04-0a60c3a445d2")
+                .param("vendorLocationsIds", "93577f24-f68f-403e-aa04-0a60c3a445d3",
+                        "93577f24-f68f-403e-aa04-0a60c3a445d4")
+                .param("tagIds", "93577f24-f68f-403e-aa04-0a60c3a445d65")
+                .param("LocalDateTime endDateTimeFrom", "2021-07-19T23:50:00")
+                .param("LocalDateTime endDateTimeTo", "2021-07-20T00:10:00")
+                .when()
+                .get("/discounts/archived")
+                .then()
+                .assertThat(MockMvcResultMatchers.status().isOk())
+                .assertThat(MockMvcResultMatchers.status().is2xxSuccessful())
+                .assertThat(jsonPath("$[0].name").value("Toy for your dog"))
+                .assertThat(jsonPath("$[0].description").value("Dog stuff"))
+                .assertThat(jsonPath("$[0].value").value("5"))
+                .assertThat(jsonPath("$[0].discountType").value("PERCENT"))
+                .assertThat(jsonPath("$[0].promo").value("1abcde1"))
+                .assertThat(jsonPath("$[0].endTime").value("2021-07-20T00:00:00"))
+                .assertThat(jsonPath("$[0].archived").value("true"))
+                .assertThat(jsonPath("$[0].vendor.name").value("Dog stuff"))
+                .assertThat(jsonPath("$[0].category.name").value("Dogs"));
+
+        given()
+                .webAppContextSetup(wac)
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .put("/discounts/archived/93577f24-f68f-403e-aa04-0a60c3a445d1/restore")
+                .then()
+                .assertThat(MockMvcResultMatchers.status().is2xxSuccessful())
+                .assertThat(jsonPath("$.name").value("Toy for your dog"))
+                .assertThat(jsonPath("$.description").value("Dog stuff"))
+                .assertThat(jsonPath("$.value").value("5"))
+                .assertThat(jsonPath("$.discountType").value("PERCENT"))
+                .assertThat(jsonPath("$.promo").value("1abcde1"))
+                .assertThat(jsonPath("$.endTime").value("2021-07-20T00:00:00"))
+                .assertThat(jsonPath("$.archived").value("false"))
+                .assertThat(jsonPath("$.vendor.name").value("Dog stuff"))
+                .assertThat(jsonPath("$.category.name").value("Dogs"));
     }
 }
